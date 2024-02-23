6149768380fb117f0031c4ab1f15147c82c8582b
Utk984 <utkarshagarwal984@gmail.com>
2024-02-24 03:06:31 +0530

    Added punctuation and spelling correction

--- process/process.js
+++ process/process.js
@@ -1,3 +1,6 @@
+var Typo = require("typo-js");
+var dictionary = new Typo("en_US");
+
 function googleSearch(query) {
   const apiKey = "AIzaSyCYzjs8J5sX4R75sI6fuN7Zwm1YyDX9tMw";
   const cx = "a11f3384e1cf243a3";
@@ -5,18 +8,40 @@ function googleSearch(query) {
   const numResults = 5;
   const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${encodedQuery}&num=${numResults}`;
 
-  // Perform the HTTP request
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      // Handle the search results
-      const links = data.items.map((item) => item.link);
-      console.log(links);
+      const results = data.items.map((item) => ({
+        title: item.title,
+        link: item.link,
+      }));
+      console.log(results);
     })
     .catch((error) => {
-      // Handle any errors
       console.error("Error fetching search results:", error);
     });
 }
 
-googleSearch("install open-cv");
+function removePunctuation(text) {
+  return text.replace(/[.,;:?!'"]/g, "");
+}
+
+function correctSpelling(prompt) {
+  let words = prompt.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    var val = dictionary.check(word);
+    if (!val) {
+      var array = dictionary.suggest(word);
+      words[i] = array[0];
+    }
+  }
+  return words.join(" ");
+}
+
+text = "I procastinate a lot, a lot.";
+text = removePunctuation(text);
+text = correctSpelling(text);
+googleSearch(text);
+console.log(text);
