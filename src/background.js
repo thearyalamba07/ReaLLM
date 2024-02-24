c05d4e0be5248dd605f4d62a5ef5e35e9d6543e0
coolperson111 <chakalguk@gmail.com>
2024-02-25 03:27:23 +0530

    first demo working lesgoooo

--- src/background.js
+++ src/background.js
@@ -1,29 +1,37 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "runFunction") {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "runFunction") {
+    const apiUrl = "https://reallm-backend.onrender.com/update_string";
 
-        const apiUrl = "https://dada-avatar.onrender.com/health";
-        var result;
-        var str1 = ""
+    const promptData = {
+      input_string: request.inputString,
+    };
 
-        // Make a GET request to the API endpoint
-        fetch(apiUrl)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                result = data
-                console.log(result.message);
-                str1 = result.message;
-                console.log(str1);
-                // sendResponse({ data });
-                sendResponse({message: str1});
+    fetch(apiUrl, {
+      method: "POST",
+      // mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+        // You might need additional headers, such as authorization headers
+      },
+      body: JSON.stringify(promptData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the optimized prompt received from the server
+        /*
+         * data.output_string contains the optimized promptData
+         * data.tokens_original contains the original token num
+         * data.tokens_processed contains the optimized promptData token num
+         * data.google_search contains the first google search link
+         */
+        console.log(data);
+        sendResponse({ message: data.output_string });
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+        sendResponse({ message: "error" });
+      });
 
-            })
-            .catch((error) => {
-                console.error("Fetch error:", error);
-                sendResponse({ message: "error"});
-            });
-
-        return true;
-    }
+    return true;
+  }
 });
