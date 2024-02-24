91e6c6619e8bb86a5c6f5d5b0dc07c78e3eaa04e
Arya <notaryalamba@gmail.com>
2024-02-25 01:05:04 +0530

    final changes

--- src/Popup/script.js
+++ src/Popup/script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var toggleSwitch = document.getElementById("toggle-switch");
-    var contentContainer = document.getElementById("content-container");
-  
-    toggleSwitch.addEventListener("change", function () {
-      if (this.checked) {
-        contentContainer.style.display = "block";
-      } else {
-        contentContainer.style.display = "none";
-      }
-    });
+  var toggleSwitch = document.getElementById("toggle-switch");
+  var dialogueWindow = document.getElementById("dialogue-window");
+
+  toggleSwitch.addEventListener("change", function () {
+    if (this.checked) {
+      dialogueWindow.style.display = "block";
+    } else {
+      dialogueWindow.style.display = "none";
+    }
   });
-  
+});
