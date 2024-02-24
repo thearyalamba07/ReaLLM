af8cc1e2175138a7ca9b4f1d13e8998bf1bf25a5
coolperson111 <chakalguk@gmail.com>
2024-02-24 13:01:49 +0530

    small changes

--- src/content_scraper.js
+++ src/content_scraper.js
@@ -1,18 +1,18 @@
-const textArea = document.getElementById('prompt-textarea');
+const textArea = document.getElementById("prompt-textarea");
 
 if (textArea) {
-    const text = textArea.textContent;
-    var wordCount = 0
-    
-    const badge = document.createElement('p');
-    badge.textContent = `how many words????  ${wordCount}`;
-    textArea.insertAdjacentElement('afterend', badge);
+  const text = textArea.textContent;
+  var wordCount = 0;
 
-    textArea.addEventListener('input', function() {
-        const text = textArea.value;
-        const words = text.match(/[^\s]+/g);
-        wordCount = words ? words.length : 0;
+  const badge = document.createElement("p");
+  badge.textContent = `how many words????  ${wordCount}`;
+  textArea.insertAdjacentElement("afterend", badge);
 
-        badge.textContent = `Word count: ${wordCount}`;
-    });
+  textArea.addEventListener("input", function () {
+    const text = textArea.value;
+    const words = text.match(/[^\s]+/g);
+    wordCount = words ? words.length : 0;
+
+    badge.textContent = `Word count: ${wordCount}`;
+  });
 }
