385929914234cfa37a5493fe1d523010b9e8d374
coolperson111 <chakalguk@gmail.com>
2024-02-25 11:45:16 +0530

    changes to background

--- src/background.js
+++ src/background.js
@@ -5,13 +5,12 @@ chrome.storage.local.get(["tokenCount"], function (result) {
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
   if (request.action === "runFunction") {
     const apiUrl = "https://reallm-backend.onrender.com/update_string";
-
     const promptData = {
       input_string: request.inputString,
     };
-
     fetch(apiUrl, {
       method: "POST",
       headers: {
@@ -21,9 +20,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     })
       .then((response) => response.json())
       .then((data) => {
-        // Handle the optimized prompt received from the server
-        /*
-         * data.output_string contains the optimized promptData
+        /* data.output_string contains the optimized promptData
          * data.tokens_original contains the original token num
          * data.tokens_processed contains the optimized promptData token num
          * data.google_search contains the first google search link
@@ -32,7 +29,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         sendResponse({
           message: data.output_string,
           token:
-            parseInt(data.tokens_original) - parseInt(data.tokens_processed),
+            parseInt(data.tokens_original, 10) - parseInt(data.tokens_processed, 10),
         });
       })
       .catch((error) => {
