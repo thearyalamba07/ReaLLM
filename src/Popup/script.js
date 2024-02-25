49bac46b9184ab28bb4f4ccf56620f39251b4d5e
Utk984 <utkarshagarwal984@gmail.com>
2024-02-25 17:20:35 +0530

    Made minor changes

--- src/Popup/script.js
+++ src/Popup/script.js
@@ -1,32 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var toggleSwitch = document.getElementById("toggle-switch");
-//   var dialogueWindow = document.getElementById("dialogue-window");
-//   var tokenSaved = document.getElementById("token-count");
-
-//   function turnOn() {
-//     dialogueWindow.textContent = `on`;
-//     chrome.runtime.sendMessage(
-//       { action: "getTokenCount" },
-//       function (response) {
-//         const num_tokens = response.num;
-//         tokenSaved.textContent = `Tokens saved: ${num_tokens}`;
-//       },
-//     );
-//   }
-
-//   turnOn();
-//   dialogueWindow.style.display = "block";
-
-//   toggleSwitch.addEventListener("change", function () {
-//     if (this.checked) {
-//       turnOn();
-//     } else {
-//       tokenSaved.textContent = `...`;
-//       dialogueWindow.textContent = `...`;
-//     }
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   var toggleSwitch = document.getElementById("toggle-switch");
   var dialogueWindow = document.getElementById("dialogue-window");
@@ -64,9 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "displayPromptContent") {
-    document.getElementById("dialogue-window").innerText =
-      request.promptContent;
-  }
-});
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.action === "displayPromptContent") {
+//     document.getElementById("dialogue-window").innerText =
+//       request.promptContent;
+//   }
+// });
