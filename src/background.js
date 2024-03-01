0e93167b4e500f6b69f6bdab4397842ef50dd686
coolperson111 <chakalguk@gmail.com>
2024-03-01 12:03:00 +0530

    basic commands testing

--- src/background.js
+++ src/background.js
@@ -51,3 +51,18 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     sendResponse({ num: tokenCount });
   }
 });
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command: ${command}`);
+    /*
+    if (command === "sendPrompt") {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "runFunction" }, function (
+          response
+        ) {
+          console.log(response);
+        });
+      });
+    }
+    */
+});
