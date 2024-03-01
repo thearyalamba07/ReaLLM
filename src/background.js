8f6093e65e051fb23074f508a9615a44b9e12dca
coolperson111 <chakalguk@gmail.com>
2024-03-02 02:11:35 +0530

    added shortcut to insert processed prompt to text area ctrl+shift+y

--- src/background.js
+++ src/background.js
@@ -1,68 +1,62 @@
 let tokenCount = 0;
 
-chrome.storage.local.get(["tokenCount"], function (result) {
-  tokenCount = result.tokenCount || 0;
+chrome.storage.local.get(["tokenCount"], function(result) {
+    tokenCount = result.tokenCount || 0;
 });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "runFunction") {
-    // const apiUrl = "https://reallm-backend.onrender.com/update_string";
-    const apiUrl = "http://127.0.0.1:8000/update_string";
-    const promptData = {
-      input_string: request.inputString,
-    };
-    fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(promptData),
-      type: "no-cors",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        /* data.output_string contains the optimized promptData
-         * data.tokens_original contains the original token num
-         * data.tokens_processed contains the optimized promptData token num
-         * data.google_search contains the first google search link
-         */
-        console.log(data);
-        sendResponse({
-          message: data.output_string,
-          token:
-            parseInt(data.tokens_original, 10) -
-            parseInt(data.tokens_processed, 10),
-        });
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-        sendResponse({ message: "error" });
-      });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "runFunction") {
+        // const apiUrl = "https://reallm-backend.onrender.com/update_string";
+        const apiUrl = "http://127.0.0.1:8000/update_string";
+        const promptData = {
+            input_string: request.inputString,
+        };
+        fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(promptData),
+            type: "no-cors",
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                /* data.output_string contains the optimized promptData
+                 * data.tokens_original contains the original token num
+                 * data.tokens_processed contains the optimized promptData token num
+                 * data.google_search contains the first google search link
+                 */
+                console.log(data);
+                sendResponse({
+                    message: data.output_string,
+                    token:
+                        parseInt(data.tokens_original, 10) -
+                        parseInt(data.tokens_processed, 10),
+                });
+            })
+            .catch((error) => {
+                console.error("Fetch error:", error);
+                sendResponse({ message: "error" });
+            });
 
-    return true;
-  }
+        return true;
+    }
 
-  if (request.action === "updateTokenCount") {
-    tokenCount += request.numTokens;
-    chrome.storage.local.set({ tokenCount });
-  }
+    if (request.action === "updateTokenCount") {
+        tokenCount += request.numTokens;
+        chrome.storage.local.set({ tokenCount });
+    }
 
-  if (request.action === "getTokenCount") {
-    sendResponse({ num: tokenCount });
-  }
+    if (request.action === "getTokenCount") {
+        sendResponse({ num: tokenCount });
+    }
 });
 
 chrome.commands.onCommand.addListener((command) => {
-    console.log(`Command: ${command}`);
-    /*
     if (command === "sendPrompt") {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "runFunction" }, function (
-          response
-        ) {
-          console.log(response);
+        console.log(`Command: ${command}`);
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "insertNewPrompt" });
         });
-      });
     }
-    */
 });
--- src/content_scraper.js
+++ src/content_scraper.js
@@ -1,143 +1,157 @@
 const textArea = document.getElementById("prompt-textarea");
 
 if (textArea) {
-  const separator = document.createElement("hr");
-  separator.style.borderTop = "1px solid white";
-  separator.style.width = "87%";
-  separator.style.marginLeft = "auto";
-  separator.style.marginRight = "auto";
-  textArea.insertAdjacentElement("afterend", separator);
+    const separator = document.createElement("hr");
+    separator.style.borderTop = "1px solid white";
+    separator.style.width = "87%";
+    separator.style.marginLeft = "auto";
+    separator.style.marginRight = "auto";
+    textArea.insertAdjacentElement("afterend", separator);
 
-  const badgeContainer = document.createElement("div");
-  badgeContainer.style.display = "flex";
-  badgeContainer.style.flexDirection = "column"; // Stack elements vertically
-  badgeContainer.style.alignItems = "flex-start"; // Align items to the start
-  badgeContainer.style.paddingLeft = "52px";
-  separator.insertAdjacentElement("afterend", badgeContainer);
+    const badgeContainer = document.createElement("div");
+    badgeContainer.style.display = "flex";
+    badgeContainer.style.flexDirection = "column"; // Stack elements vertically
+    badgeContainer.style.alignItems = "flex-start"; // Align items to the start
+    badgeContainer.style.paddingLeft = "52px";
+    separator.insertAdjacentElement("afterend", badgeContainer);
 
-  const tokbadge = document.createElement("p");
-  tokbadge.textContent = `Tokens saved:`;
-  tokbadge.style.color = "white";
-  tokbadge.style.margin = "2";
-  tokbadge.style.paddingLeft = "6px";
-  tokbadge.style.fontWeight = "400";
-  tokbadge.style.fontSize = "14px";
-  badgeContainer.appendChild(tokbadge); // Add the token badge to the container
+    const tokbadge = document.createElement("p");
+    tokbadge.textContent = `Tokens saved:`;
+    tokbadge.style.color = "white";
+    tokbadge.style.margin = "2";
+    tokbadge.style.paddingLeft = "6px";
+    tokbadge.style.fontWeight = "400";
+    tokbadge.style.fontSize = "14px";
+    badgeContainer.appendChild(tokbadge); // Add the token badge to the container
 
-  const processedContainer = document.createElement("div"); // Create a container for the badge and button
-  processedContainer.style.display = "flex"; // Set the display to flex for horizontal alignment
-  processedContainer.style.alignItems = "center"; // Align items vertically in the center
-  badgeContainer.appendChild(processedContainer); // Add the processed container to the badge container
+    const processedContainer = document.createElement("div"); // Create a container for the badge and button
+    processedContainer.style.display = "flex"; // Set the display to flex for horizontal alignment
+    processedContainer.style.alignItems = "center"; // Align items vertically in the center
+    badgeContainer.appendChild(processedContainer); // Add the processed container to the badge container
 
-  const arrowButton = document.createElement("button");
-  arrowButton.innerHTML = "&#x27A4;";
-  arrowButton.style.border = "none";
-  arrowButton.style.background = "transparent";
-  arrowButton.style.color = "red";
-  arrowButton.style.fontSize = "16px";
-  arrowButton.style.cursor = "pointer";
-  arrowButton.style.padding = "6px 8px";
-  processedContainer.appendChild(arrowButton); // Add the button to the processed container
+    const arrowButton = document.createElement("button");
+    arrowButton.innerHTML = "&#x27A4;";
+    arrowButton.style.border = "none";
+    arrowButton.style.background = "transparent";
+    arrowButton.style.color = "red";
+    arrowButton.style.fontSize = "16px";
+    arrowButton.style.cursor = "pointer";
+    arrowButton.style.padding = "6px 8px";
+    processedContainer.appendChild(arrowButton); // Add the button to the processed container
 
-  const badge = document.createElement("p");
-  badge.textContent = `Processed prompt: `;
-  badge.style.color = "white";
-  badge.style.margin = "0";
-  badge.style.fontWeight = "400";
-  badge.style.fontSize = "14px";
-  processedContainer.appendChild(badge);
+    const badge = document.createElement("p");
+    badge.textContent = `Processed prompt: `;
+    badge.style.color = "white";
+    badge.style.margin = "0";
+    badge.style.fontWeight = "400";
+    badge.style.fontSize = "14px";
+    processedContainer.appendChild(badge);
 
-  const googleButton = document.createElement("button");
-  googleButton.innerHTML = "GOOGLE";
-  badgeContainer.insertAdjacentElement("afterend", googleButton);
+    const googleButton = document.createElement("button");
+    googleButton.innerHTML = "GOOGLE";
+    badgeContainer.insertAdjacentElement("afterend", googleButton);
 
-  let timeoutId;
+    let timeoutId;
 
-  function handleInput() {
-    clearTimeout(timeoutId);
-    console.log("input");
-    timeoutId = setTimeout(() => {
-      const text = textArea.value;
-      chrome.runtime.sendMessage(
-        { action: "runFunction", inputString: text },
-        function (response) {
-          prompt = response.message;
-          num_tokens = response.token;
-          updateTokBadge(num_tokens);
-          badge.textContent = `Processed prompt: ${prompt}`;
-        },
-      );
-    }, 1000);
-  }
-
-  textArea.addEventListener("input", handleInput);
-
-  textArea.addEventListener("input", function () {
-    const inputData = event.data;
-    if (inputData === " " || inputData.match(/[?.,!;:()]/)) {
-      const text = textArea.value;
-      chrome.runtime.sendMessage(
-        { action: "runFunction", inputString: text },
-        function (response) {
-          prompt = response.message;
-          num_tokens = response.token;
-          updateTokBadge(num_tokens);
-          badge.textContent = `Processed prompt: ${prompt}`;
-        },
-      );
+    function handleInput() {
+        clearTimeout(timeoutId);
+        console.log("input");
+        timeoutId = setTimeout(() => {
+            const text = textArea.value;
+            chrome.runtime.sendMessage(
+                { action: "runFunction", inputString: text },
+                function(response) {
+                    prompt = response.message;
+                    num_tokens = response.token;
+                    updateTokBadge(num_tokens);
+                    badge.textContent = `Processed prompt: ${prompt}`;
+                },
+            );
+        }, 1000);
     }
-  });
-  textArea.addEventListener("keydown", function (event) {
-    if (
-      event.key === "Backspace" ||
-      event.key === "Delete" ||
-      event.key === "Enter"
-    ) {
-      setTimeout(() => {
-        const text = textArea.value;
-        chrome.runtime.sendMessage(
-          { action: "runFunction", inputString: text },
-          function (response) {
-            prompt = response.message;
-            num_tokens = response.token;
-            updateTokBadge(num_tokens);
-            badge.textContent = `Processed prompt: ${prompt}`;
-          },
-        );
-      }, 0);
+
+    textArea.addEventListener("input", handleInput);
+
+    textArea.addEventListener("input", function() {
+        const inputData = event.data;
+        if (inputData === " " || inputData.match(/[?.,!;:()]/)) {
+            const text = textArea.value;
+            chrome.runtime.sendMessage(
+                { action: "runFunction", inputString: text },
+                function(response) {
+                    prompt = response.message;
+                    num_tokens = response.token;
+                    updateTokBadge(num_tokens);
+                    badge.textContent = `Processed prompt: ${prompt}`;
+                },
+            );
+        }
+    });
+    textArea.addEventListener("keydown", function(event) {
+        if (
+            event.key === "Backspace" ||
+            event.key === "Delete" ||
+            event.key === "Enter"
+        ) {
+            setTimeout(() => {
+                const text = textArea.value;
+                chrome.runtime.sendMessage(
+                    { action: "runFunction", inputString: text },
+                    function(response) {
+                        prompt = response.message;
+                        num_tokens = response.token;
+                        updateTokBadge(num_tokens);
+                        badge.textContent = `Processed prompt: ${prompt}`;
+                    },
+                );
+            }, 0);
+        }
+    });
+
+    function insertProcessedPrompt() {
+        if (textArea.value === "") {
+            alert("Enter a prompt");
+        } else if (badge.textContent === "Processed prompt: ") {
+            alert("Processing... Please wait.");
+        } else {
+            const processedPrompt = badge.textContent.replace(
+                "Processed prompt: ",
+                "",
+            );
+            textArea.value = processedPrompt;
+            const num_tokens = parseInt(tokbadge.textContent.substring(14), 10);
+            chrome.runtime.sendMessage({
+                action: "updateTokenCount",
+                numTokens: num_tokens,
+            });
+            badge.textContent = "Processed prompt: ";
+            tokbadge.textContent = "Tokens saved: ";
+        }
+
     }
-  });
 
-  arrowButton.addEventListener("click", function () {
-    if (badge.textContent.includes("Processing") || badge.textContent == "") {
-      alert("Processing... Please wait.");
-    } else {
-      const processedPrompt = badge.textContent.replace(
-        "Processed prompt: ",
-        "",
-      );
-      textArea.value = processedPrompt;
-      const num_tokens = parseInt(tokbadge.textContent.substring(14), 10);
-      chrome.runtime.sendMessage({
-        action: "updateTokenCount",
-        numTokens: num_tokens,
-      });
-      badge.textContent = "Processed prompt:";
-      tokbadge.textContent = "Tokens saved:";
+    arrowButton.addEventListener("click", function() {
+        event.preventDefault();
+        insertProcessedPrompt();
+    });
+
+    googleButton.addEventListener("click", function() {
+        event.preventDefault();
+        const processedPrompt = badge.textContent.replace("Processed prompt: ", "");
+        const url = `https://www.google.com/search?q=${processedPrompt}`;
+        window.open(url, "_blank");
+        textArea.value = "";
+        badge.textContent = "Processed prompt:";
+        tokbadge.textContent = "Tokens saved:";
+    });
+
+    function updateTokBadge(tokenCount) {
+        tokbadge.textContent = `Tokens saved: ${tokenCount}`;
     }
-  });
 
-  googleButton.addEventListener("click", function () {
-    event.preventDefault();
-    const processedPrompt = badge.textContent.replace("Processed prompt: ", "");
-    const url = `https://www.google.com/search?q=${processedPrompt}`;
-    window.open(url, "_blank");
-    textArea.value = "";
-    badge.textContent = "Processed prompt:";
-    tokbadge.textContent = "Tokens saved:";
-  });
-
-  function updateTokBadge(tokenCount) {
-    tokbadge.textContent = `Tokens saved: ${tokenCount}`;
-  }
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === "insertNewPrompt") {
+            insertProcessedPrompt();
+        }
+    });
 }
